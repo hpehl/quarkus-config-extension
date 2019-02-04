a0051c1e7d5b4bc4b07cccace3fedd5f12ef1205
Harald Pehl <harald.pehl@gmail.com>
2019-02-04 10:31:20 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,38 @@
+# Shamrock Config Extension
+
+Adds a servlet to a Protean application which lists the configured values for [MicroProfile Configuration](https://microprofile.io/project/eclipse/microprofile-config). The servlet is registered using the path `/config` by default and lists all properties of all config sources. The config sources are sorted descending by ordinal, the properties by name. If no config is defined an empty JSON object is returned. 
+
+A typical output might look like:
+```json
+{
+  "sources": [
+    {
+      "source": "SysPropConfigSource",
+      "ordinal": 400,
+      "properties": {
+        "file.encoding": "UTF-8",
+        "file.separator": "/",
+        ...
+      }
+    },
+    {
+      "source": "EnvConfigSource",
+      "ordinal": 300,
+      "properties": {
+        "EDITOR": "vim",
+        "LC_ALL": "en_US.UTF-8",
+        ...
+      }
+    },
+    {
+      "source": "PropertiesConfigSource[source=resource:META-INF/microprofile-config.properties]",
+      "ordinal": 100,
+      "properties": {
+        "greeting.message": "hello",
+        "greeting.name": "shamrock",
+        ...
+      }
+    }
+  ]
+}
+```
---
+++ deployment/src/main/java/org/jboss/shamrock/config/ConfigProcessor.java
@@ -0,0 +1,63 @@
+/*
+ * Copyright 2019 Red Hat, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jboss.shamrock.config;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.jboss.shamrock.arc.deployment.AdditionalBeanBuildItem;
+import org.jboss.shamrock.deployment.annotations.BuildStep;
+import org.jboss.shamrock.deployment.builditem.FeatureBuildItem;
+import org.jboss.shamrock.runtime.annotations.ConfigGroup;
+import org.jboss.shamrock.runtime.annotations.ConfigItem;
+import org.jboss.shamrock.undertow.ServletBuildItem;
+
+/**
+ * Provides a servlet which lists all configured properties.
+ *
+ * @author Harald Pehl
+ */
+class ConfigProcessor {
+
+    /** The configuration for health checking. */
+    Config config;
+
+    @ConfigGroup
+    static final class Config {
+        /** The path of the config servlet. */
+        @ConfigItem(defaultValue = "/config")
+        String path;
+    }
+
+    @BuildStep
+    ServletBuildItem produceServlet() {
+        ServletBuildItem servletBuildItem = new ServletBuildItem("config", ConfigServlet.class.getName());
+        servletBuildItem.getMappings().add(config.path);
+        return servletBuildItem;
+    }
+
+    @BuildStep
+    List<AdditionalBeanBuildItem> additionalBeans() {
+        return Arrays.asList(
+                new AdditionalBeanBuildItem(Config.class),
+                new AdditionalBeanBuildItem(ConfigServlet.class));
+    }
+
+    @BuildStep
+    FeatureBuildItem feature() {
+        return new FeatureBuildItem("config");
+    }
+}
